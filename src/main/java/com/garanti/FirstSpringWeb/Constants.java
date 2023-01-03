a424006400f72321dc5dd7a3eeb8a883daca6462
Hüseyin Balicak <hsyn.balicak@gmail.com>
2023-01-03 23:41:34 +0300

    fixed oracle database configuration settings

--- src/main/java/com/garanti/FirstSpringWeb/Constants.java
+++ src/main/java/com/garanti/FirstSpringWeb/Constants.java
@@ -2,15 +2,14 @@ package com.garanti.FirstSpringWeb;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public final class Constants
 {
-    private static final String USER = "BILGE";
+    public static final String USER = "BILGE";
 
-    private static final String PASSWORD = "123456";
+    public static final String PASSWORD = "123456";
 
-    private static final String URL = "jdbc:oracle:thin:@localhost:1521";
+    public static final String URL = "jdbc:oracle:thin:@localhost:1521";
 
     public static Connection getConnection() throws Exception
     {
