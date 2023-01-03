b598bfe4663adb1f005b0ec85ca60302e5fbcb33
Hüseyin Balicak <hsyn.balicak@gmail.com>
2023-01-04 00:38:10 +0300

    fixed DersOgrenciController

--- src/main/java/com/garanti/FirstSpringWeb/controller/DersOgrenciController.java
+++ src/main/java/com/garanti/FirstSpringWeb/controller/DersOgrenciController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "dersogrenci")
@@ -23,111 +23,75 @@ public class DersOgrenciController {
 
     private Ders_OgrenciRepo repo;
 
-    public DersOgrenciController()
-    {
-        this.repo = new Ders_OgrenciRepo();
+    public DersOgrenciController(Ders_OgrenciRepo repo) {
+        this.repo = repo;
     }
 
+
     @GetMapping(path = "getAll", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ArrayList<Ders_Ogrenci>> getAll()
-    {
-        // localhost:9090/FirstRestfulService/dersogrenci/getAll
-        ArrayList<Ders_Ogrenci> res = repo.getAll();
-        if (res == null || res.size() == 0)
-        {
+    public ResponseEntity<List<Ders_Ogrenci>> getAll() {
+        List<Ders_Ogrenci> res = repo.getAll();
+        if (res == null || res.size() == 0) {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-        }
-        else
-        {
+        } else {
             return ResponseEntity.ok(res);
         }
     }
 
     @GetMapping(path = "getByIdHeader", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Ders_Ogrenci> getByIdHeader(@RequestHeader(name = "id") Integer id)
-    {
-        // localhost:9090/FirstRestfulService/dersogrenci/getById?id=1
+    public ResponseEntity<Ders_Ogrenci> getByIdHeader(@RequestHeader(name = "id") Integer id) {
         Ders_Ogrenci res = repo.getById(id);
-        if (res != null)
-        {
+        if (res != null) {
             return ResponseEntity.ok(res);
-        }
-        else
-        {
+        } else {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
     }
 
     @GetMapping(path = "getById", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Ders_Ogrenci> getByIdQueryParam(@RequestParam(value = "id", required = true) Integer id)
-    {
-        // localhost:9090/FirstRestfulService/dersogrenci/getById?id=1
+    public ResponseEntity<Ders_Ogrenci> getByIdQueryParam(@RequestParam(value = "id", required = true) Integer id) {
         Ders_Ogrenci res = repo.getById(id);
-        if (res != null)
-        {
+        if (res != null) {
             return ResponseEntity.ok(res);
-        }
-        else
-        {
+        } else {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
     }
 
     @GetMapping(path = "getById/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Ders_Ogrenci> getByIdPathParam(@PathVariable(value = "id") Integer id)
-    {
-        // localhost:9090/FirstRestfulService/dersogrenci/getById/1
+    public ResponseEntity<Ders_Ogrenci> getByIdPathParam(@PathVariable(value = "id") Integer id) {
 
         Ders_Ogrenci res = repo.getById(id);
-        if (res != null)
-        {
+        if (res != null) {
             return ResponseEntity.ok(res);
-        }
-        else
-        {
+        } else {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
     }
 
     @PostMapping(path = "save", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> save(@RequestBody Ders_Ogrenci dersogrenci)
-    {
-        // localhost:9090/FirstRestfulService/dersogrenci/save
-        // {"name":"RestTest", "is_GICIK": true}
-        if (repo.save(dersogrenci))
-        {
+    public ResponseEntity<String> save(@RequestBody Ders_Ogrenci dersogrenci) {
+        if (repo.save(dersogrenci)) {
             return ResponseEntity.status(HttpStatus.CREATED).body("Başarı ile kaydedildi");
-        }
-        else
-        {
+        } else {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Başarı ile kaydedildi");
         }
     }
 
     @DeleteMapping(path = "deleteById/{id}")
-    public ResponseEntity<String> deleteById(@PathVariable(value = "id") Integer id)
-    {
-        // localhost:9090/FirstRestfulService/dersogrenci/deleteById/1
-        if (repo.deleteById(id))
-        {
+    public ResponseEntity<String> deleteById(@PathVariable(value = "id") Integer id) {
+        if (repo.deleteById(id)) {
             return ResponseEntity.ok("Başarı ile silindi");
-        }
-        else
-        {
+        } else {
             return ResponseEntity.internalServerError().body("Başarı ile silinemedi");
         }
     }
 
     @DeleteMapping(path = "deleteByIdHeader")
-    public ResponseEntity<String> deleteByIdHeader(@RequestHeader(value = "id") Integer id)
-    {
-        // localhost:9090/FirstRestfulService/dersogrenci/deleteById/1
-        if (repo.deleteById(id))
-        {
+    public ResponseEntity<String> deleteByIdHeader(@RequestHeader(value = "id") Integer id) {
+        if (repo.deleteById(id)) {
             return ResponseEntity.ok("Başarı ile silindi");
-        }
-        else
-        {
+        } else {
             return ResponseEntity.internalServerError().body("Başarı ile silinemedi");
         }
     }
